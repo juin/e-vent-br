27c184abbc730c17b216fa7d1e00492faa0328c6
cremildolima@gmail.com <cremildolima@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-02-08 18:22:33 +0000

    Alteração dos nomes das classe para o novo padrão com 3 camadas.

--- trunk/aplicativo/fachada/FachadaUsuario.php
+++ trunk/aplicativo/fachada/FachadaUsuario.php
@@ -1,25 +1,24 @@
 <?php
-//require_once('../../modelo/ModeloUsuario.php');
+/**
+ * Inclui a classe UsuarioSessao, 
+ * responsável por retornar os dados do usuário criado na sessão.
+ */
 require_once('../../classes/UsuarioSessao.php');
 
 /**
- * Controle responsável pelas definições do usuário;
+ * Classe responsável pelas definições do usuário;
  */
-class ControleUsuario{
+class FachadaUsuario{
 
 	public function validarAcesso($login, $senha){
-        
         $usuario = new UsuarioSessao();
-        
         $usuario->setCod_usuario("3");
         $usuario->setLogin("event");
         $usuario->setNome("Junior");
         $usuario->setSenha("123");
-        
         return $usuario;
-        
-	}
+	}//Fim da função validarAcesso($login, $senha)
 
-}
+}//Fim da Classe FachadaUsuario
 
 ?>
