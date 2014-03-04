71c6ecc8ef126ef90b7cb125f4c806da95e17f2b
cremildolima@gmail.com <cremildolima@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-03-04 15:45:19 +0000

    Criado metodo para mostrar menu de acordo com o nivel do usuário.

--- trunk/aplicativo/apresentacao/index.php
+++ trunk/aplicativo/apresentacao/index.php
@@ -1,12 +1,31 @@
 <?
+require_once('Usuario/menu.php');
 session_start();
 
-if($_SESSION['usuario']!=null){
-  echo "logado.";
-  ?>
-  <a href="sair.php">Sair.</a>
-  <?
-} else {
-	header("location: Usuario/index.php");
-}
+if($_SESSION['usuario']==null):
+  header("location: Usuario/index.php");
+endif;
+
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Sistema de gerenciamento de Eventos | e-Vent-br</title>
+</head>
+
+<body>
+    <ul>
+        <li>Inicio</li>
+        <li>Eventos</li>
+        <li>Inscrições</li>
+        <li>Certificados</li>
+    </ul>
+    <ul>
+        <li>Inscrições</li>
+        <li>Certificados</li>
+        <? menu($_SESSION['nivel_acesso']); ?>
+    </ul>
+
+</body>
+</html>
