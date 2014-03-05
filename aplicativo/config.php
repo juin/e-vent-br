ab23c653b7b4263480fca112d7d4535af0a61e22
cremildolima@gmail.com <cremildolima@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-03-05 23:37:33 +0000

    Melhorias no login, retirando algumas várias que estavam sendo criadas de forma duplicada no arquivo login.php (que antes estava como index.php) dentro da pasta Usuario da camada de apresentação. No arquivo cabecalho.php estou recuperando os dados da $_SESSION['usuario'] e atribuindo a um array chamado $usuarioLogado.

---
+++ aplicativo/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("MODULO_DIRETORIO", dirname(__FILE__)."/");
+define("URL","http://localhost/e-vent/aplicativo/");
+define("CLASSES",URL."classes/");
+define("FACHADAS",URL."fachada/");
+define("PERSISTENCIAS",URL."persistencia/");
+define("APRESENTACAO",URL."apresentacao/");
+
+
+?>
