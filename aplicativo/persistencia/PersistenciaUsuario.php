5b683e3b794586f40ee2f7f950aa804309279b9b
cremildolima@gmail.com <cremildolima@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-03-16 14:09:27 +0000

    Alterado tratamento do retorno da instancia do usuario. Utilizado foreach() para criar o array com os dados do usuario logado.

--- aplicativo/persistencia/PersistenciaUsuario.php
+++ aplicativo/persistencia/PersistenciaUsuario.php
@@ -1,72 +1,75 @@
 <?php
-require_once(dirname(__FILE__).'../../config.php');
-require_once(FACHADAS.'FachadaConectorBD.php');
-require_once(CLASSES.'UsuarioSessao.php');
-require_once(CLASSES.'InstanciaUnica.php');
+require_once (dirname(__FILE__) . '../../config.php');
+require_once (FACHADAS . 'FachadaConectorBD.php');
+require_once (CLASSES . 'UsuarioSessao.php');
+require_once (CLASSES . 'InstanciaUnica.php');
 //
 //Classe que retorna os dados de usuário
-class PersistenciaUsuario extends InstanciaUnica{
-     
-	public function selecionarPorLoginSenha($login,$senha)
-	{
-		//Transformar array de dados (bidimensional) em array de objetos (unidimensional)
-        $sql = "SELECT * FROM Usuario WHERE login='" . $login . "' AND senha='" . $senha . "'";
-        
-        //Retorna do Banco array com resultados da consulta.
-        $resultado = FachadaConectorBD::getInstancia()->consultar($sql);
-        
-        //Necessário inicializar váriaves para evitar "Warnings" do PHP.
-        $i = 0;
-        $usuarios = NULL;
-        
-        //Percorre array que retornou do banco de dados e cria um objeto do tipo UsuarioSessao
-        while ($linha = mysql_fetch_array($resultado, MYSQL_ASSOC)) {
-            $usuario = new UsuarioSessao();
-            $usuario->setCodUsuario($linha['cod_usuario']);
-            $usuario->setLogin($linha['login']);
-            $usuario->setNivelAcesso($linha['nivel_acesso']);
-            $usuario->setCategoria($linha['categoria']);
-            
-            $usuarios[$i++] = $usuario;
-        }
-        
-        return $usuarios;
-	}
-	
-	public function adicionarUsuario(Usuario $usuario){
+class PersistenciaUsuario extends InstanciaUnica {
 
-		if ($usuario instanceof Usuario){
-			$cod = $usuario->getCodUsuario();
-			$nome = $usuario->getNome();
-			$sexo = $usuario->getSexo();
-			$nasc = $usuario->getNasc();
-			$cpf = $usuario->getCpf();
-			$rg = $usuario->getRg();
-			$login = $usuario->getLogin();
-			$senha = $usuario->getSenha();
-			$tel1 = $usuario->getTel1();
-			$tel2 = $usuario->getTel2();
-			$email = $usuario->getEmail();
-			$instituicao = $usuario->getInstituicao();
-			$curso = $usuario->getCurso();
-			$lattes = $usuario->getLattes();
-			$categ = $usuario->getCategoria();
-			$nivel = $usuario->getNivelAcesso();
-			$notif = $usuario->getNotifica();
-			$status = $usuario->getStatus();
-			$dtcad = $usuario->getDatacad();
-			$cidade = $usuario->getCidade();
-			
-			$sql = "Insert into Usuario values ($cod,'$nome','$sexo',$nasc,'$cpf','$rg','$login',
+    public function selecionarPorLoginSenha($login, $senha) {
+        //Transformar array de dados (bidimensional) em array de objetos (unidimensional)
+        $sql = "SELECT * FROM Usuario WHERE login='" . $login . "' AND senha='" . $senha . "'";
+
+        //Retorna do Banco array com resultados da consulta.
+        $registros = FachadaConectorBD::getInstancia() -> consultar($sql);
+
+        //Necessário inicializar váriaves para evitar "Warnings" do PHP.
+        $usuarios = NULL;
+
+        if (!is_null($registros)) {
+            
+            $i = 0;
+            //Percorre array que retornou do banco de dados e cria um objeto do tipo UsuarioSessao
+            foreach ($registros as $registro) {
+                $usuario = new UsuarioSessao();
+                $usuario -> setCodUsuario($registro['cod_usuario']);
+                $usuario -> setLogin($registro['login']);
+                $usuario -> setNivelAcesso($registro['nivel_acesso']);
+                $usuario -> setCategoria($registro['categoria']);
+
+                $usuarios[$i++] = $usuario;   
+            }
+        }
+
+        return $usuarios;
+    }
+
+    public function adicionarUsuario(Usuario $usuario) {
+
+        if ($usuario instanceof Usuario) {
+            $cod = $usuario -> getCodUsuario();
+            $nome = $usuario -> getNome();
+            $sexo = $usuario -> getSexo();
+            $nasc = $usuario -> getNasc();
+            $cpf = $usuario -> getCpf();
+            $rg = $usuario -> getRg();
+            $login = $usuario -> getLogin();
+            $senha = $usuario -> getSenha();
+            $tel1 = $usuario -> getTel1();
+            $tel2 = $usuario -> getTel2();
+            $email = $usuario -> getEmail();
+            $instituicao = $usuario -> getInstituicao();
+            $curso = $usuario -> getCurso();
+            $lattes = $usuario -> getLattes();
+            $categ = $usuario -> getCategoria();
+            $nivel = $usuario -> getNivelAcesso();
+            $notif = $usuario -> getNotifica();
+            $status = $usuario -> getStatus();
+            $dtcad = $usuario -> getDatacad();
+            $cidade = $usuario -> getCidade();
+
+            $sql = "Insert into Usuario values ($cod,'$nome','$sexo',$nasc,'$cpf','$rg','$login',
 					'$senha','$tel1','$tel2','$email','$instituicao','$curso','$lattes','$categ',
 					'$nivel','$notif','$status',$dtcad,$cidade)";
-			
-			/*$id = FachadaConectorBD::getInstancia()->inserir($sql);*/
-			$id = $sql;
-		} else {
-			$id = "Erro!";
-		}		
-		return $id;
-	}
+
+            /*$id = FachadaConectorBD::getInstancia()->inserir($sql);*/
+            $id = $sql;
+        } else {
+            $id = "Erro!";
+        }
+        return $id;
+    }
+
 }
 ?>
