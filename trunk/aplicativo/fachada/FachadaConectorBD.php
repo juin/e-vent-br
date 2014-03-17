4effdc45b4c5437de8a3609e3b6d066904a3c906
cremildolima@gmail.com <cremildolima@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-03-17 04:18:34 +0000

    Inicio da utilização da extensão mysqli, em detrimento da mysql. Será precisor alterar as outras queries para o novo padrão.

--- trunk/aplicativo/fachada/FachadaConectorBD.php
+++ trunk/aplicativo/fachada/FachadaConectorBD.php
@@ -1,105 +1,110 @@
 <?
-require_once(CLASSES.'ParametrosAcessoBanco.php');
+require_once (CLASSES . 'ParametrosAcessoBanco.php');
 /**
  * Configurações do Banco de Dados
  */
- 
-class FachadaConectorBD{
-    
+
+class FachadaConectorBD {
+
     //Criar Instância da classe
     private static $instancia = NULL;
-    
+
     //Atributo para guardar os parametros do Banco de Dados
     private $parametros = NULL;
-    
-    public static function iniciarInstancia($parametrosBD){
+
+    public static function iniciarInstancia($parametrosBD) {
         if (!isset(self::$instancia)) {
             $c = __CLASS__;
             self::$instancia = new $c;
             self::$instancia->setParametrosAcessoBanco($parametrosBD);
         }
     }
-    
+
     //O método singleton
-    public static function getInstancia()
-    {
+    public static function getInstancia() {
         return self::$instancia;
     }
-    
+
     /**
      * Set que recebe instancia de ParametrosAcessoBanco
      */
-    public function setParametrosAcessoBanco($parametrosBD){
-        $this->parametros = $parametrosBD;     
+    public function setParametrosAcessoBanco($parametrosBD) {
+        $this -> parametros = $parametrosBD;
     }
+
     /**
-      * Método que conecta no banco de dados
-      */
-    public function conectarBD(){
-       return mysql_connect($this->parametros->getBDServidor(),$this->parametros->getBDUsuario(),$this->parametros->getBDSenha())
-            or die('Não foi possível conectar: ' . mysql_error());
+     * Método que conecta e SELECIONA o banco de dados
+     */
+    public function conectarBD() {
+        $mysqli = new mysqli($this->parametros->getBDServidor(),$this->parametros->getBDUsuario(), $this->parametros->getBDSenha(),$this->parametros->getBDNomeBanco());
+        /* check connection */
+        if (mysqli_connect_errno()) {
+            printf("Falha na Conexão: %s\n", mysqli_connect_error());
+            exit();
+        }
+        return $mysqli;
     }
-    
+
     /**
-      * Método que seleciona o banco de dados
-      */
-    public function selecionarBD(){
-       return mysql_select_db($this->parametros->getBDNomeBanco())
-            or die('Não foi possível selecionar o Banco de Dados'. mysql_error());
+     * Método que executa uma consulta no banco de dados
+     */
+    public function consultar($query) {
+        $mysqli = $this->conectarBD();
+        $res = $mysqli->query($query); 
+        $registros = null;
+        $i = 0;
+        
+        
+        while ($saida = $res->fetch_array(MYSQLI_NUM)) {
+            $registros[$i] = $saida;
+            $i++;
+        }
+        $mysqli->close();
+        return $registros;
     }
-    
-    /**
-      * Método que executa uma consulta no banco de dados
-      */
-    public function consultar($query){
-      $this->conectarBD();
-      $this->selecionarBD();
-      $res = mysql_query($query) or die('Não foi possível consultar o Banco de Dados' . mysql_error());
-      $registros = null;
-      $i = 0;
-      while ($saida = mysql_fetch_array($res)){
-      	$registros[$i] = $saida;
-      	$i++;
-      }
-      mysql_close();
-      return $registros;
-   }
-   
-   public function inserir($query){
-   		$this->conectarBD();
-   		$this->selecionarBD();
-   		mysql_query($query) or die('Não foi possível inserir no Banco de Dados'.mysql_errno());
-   		$id = $mysql_insert_id;
-   		mysql_close();
-   		return $id;
-   }
-   
-   public function executarTransacao($queries){
-       $this->conectarBD();
-       $this->selecionarBD();
-       $comit = 'SET AUTOCOMMIT=0; START TRANSACTION;';
-       foreach ($queries as $query) {
-           $comit .= $query;
-           echo $comit . "<br>";
-       }
-       $comit .='COMMIT;';
-       echo "<br>FINAL: " . $comit;
-       mysql_query($comit) or die('<br>Não foi possível inserir no Banco de Dados'.mysql_errno().mysql_error());
-       mysql_close();
-       
-   }
-   
+
+    public function inserir($query) {
+        $this -> conectarBD();
+        $this -> selecionarBD();
+        mysql_query($query) or die('Não foi possível inserir no Banco de Dados' . mysql_errno());
+        $id = $mysql_insert_id;
+        mysql_close();
+        return $id;
+    }
+
+    public function executarTransacao($queries) {
+        $mysqli = $this->conectarBD();
+        $comit = "";
+        $mysqli->autocommit(FALSE);
+        
+        foreach ($queries as $query) {
+            $comit .= $query;
+            //$mysqli->query($query);
+        }
+        $mysqli->multi_query($comit);
+        /* commit transaction */
+        if (!$mysqli->commit()) {
+            print("Transaction commit failed\n");
+            exit();
+        }
+        /* close connection */
+        $mysqli->close();
+        
+
+    }
+
 }
+
 /**
  * Parametros de Acesso ao banco de dados
  * Cremildo: Verificar melhor forma de refazer esse processo. (Usar Properties)
  */
-if(FachadaConectorBD::getInstancia() == NULL){
+if (FachadaConectorBD::getInstancia() == NULL) {
     $parametrosBD = new ParametrosAcessoBanco();
-    $parametrosBD->setBDServidor('localhost');
-    $parametrosBD->setBDUsuario('e_vent');
-    $parametrosBD->setBDSenha('3v3nt');
-    $parametrosBD->setBDNomeBanco('e_event_br');  
+    $parametrosBD -> setBDServidor('localhost');
+    $parametrosBD -> setBDUsuario('e_vent');
+    $parametrosBD -> setBDSenha('3v3nt');
+    $parametrosBD -> setBDNomeBanco('e_event_br');
     FachadaConectorBD::iniciarInstancia($parametrosBD);
 }
 ?>
