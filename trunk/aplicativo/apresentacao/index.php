14803be1658248bd81cade597ef43dcae24a5b04
cremildolima@gmail.com <cremildolima@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-03 04:11:37 +0000

    Implementção de metódos para listar todas as inscrições do usuário Logado.

--- trunk/aplicativo/apresentacao/index.php
+++ trunk/aplicativo/apresentacao/index.php
@@ -1,21 +1,46 @@
-<? 
-require_once(dirname(__FILE__).'/../config.php');
-require_once(APRESENTACAO.'cabecalho.php');
+<?
+require_once (dirname(__FILE__) . '/../config.php');
+require_once (APRESENTACAO . 'cabecalho.php');
 ?>
+<ul>
+    <li>
+        Inicio
+    </li>
+    <li>
+        Eventos
+    </li>
+</ul>
+<ul>
+    <? menu($usuarioLogado -> getNivelAcesso()); ?>
+</ul>
+<fieldset>
+    <legend>
+        Eventos em Andamentos
+    </legend>
+</fieldset>
+<fieldset>
+    <legend>
+        Eventos Encerrados
+    </legend>
+</fieldset>
+<fieldset>
+    <legend>
+        Inscrições
+    </legend>
+    <?
+        $inscricoes = FachadaUsuario::getInstancia() -> listarInscricoes($usuarioLogado -> getCodUsuario());
+    ?>
     <ul>
-        <li>Inicio</li>
-        <li>Eventos</li>
-    </ul>
-    <ul>
-        <? menu($usuarioLogado->getNivelAcesso()); ?>
-    </ul>
-    <fieldset>
-        <legend>Eventos em Andamentos</legend>
-    </fieldset>
-    <fieldset>
-        <legend>Eventos Encerrados</legend>
-    </fieldset>
-    <fieldset>
-        <legend>Inscrições</legend>
-    </fieldset>
-<? require_once('rodape.php'); ?>
+        <?
+        if ($inscricoes!=NULL) {
+            foreach ($inscricoes as $inscricao) {
+                $evento = FachadaEvento::getInstancia() -> selecionaEventoPorCodigo($inscricao['cod_evento']);
+                echo "<li>Código Inscrição: " . $inscricao['cod_inscricao'] . " Evento: " . $evento -> getNome() . " Status: " . $inscricao['status'];
+
+            }
+        }
+        ?>
+</fieldset>
+<?
+    require_once ('rodape.php');
+ ?>
