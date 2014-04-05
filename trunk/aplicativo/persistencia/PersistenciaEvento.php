a740ad2a2a8b9ff3719b7091b2dfa37c3bc9c355
luispscarvalho@gmail.com <luispscarvalho@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-05 19:08:30 +0000

    + transferencia de resultset para colecao de objetos

--- trunk/aplicativo/persistencia/PersistenciaEvento.php
+++ trunk/aplicativo/persistencia/PersistenciaEvento.php
@@ -3,18 +3,45 @@ require_once (FACHADAS.'FachadaConectorBD.php');
 
 class PersistenciaEvento extends InstanciaUnica{
 	
-	public function selecionarListaEventos($status){
+	public function selecionarEventosPorStatus($status){
+		$eventos = NULL;	
 		$sql = "Select cod_evento, nome, sigla, data_inicio, status, pagamento from Evento WHERE status LIKE '".$status."'";
-		$res = FachadaConectorBD::getInstancia()->consultar($sql);
-		return $res;
+		$registros = FachadaConectorBD::getInstancia()->consultar($sql);
+		$i = 0;
+		if (!is_null($registros)){
+			foreach ($registros as $registro){
+				$eventos[$i] = new Evento();
+				$eventos[$i]->setCodevento($registro["cod_evento"]);
+				$eventos[$i]->setNome($registro["nome"]);
+				$eventos[$i]->setSigla($registro["sigla"]);
+				$eventos[$i]->setDatainicio($registro["data_inicio"]);
+				$eventos[$i]->setStatus($registro["status"]);
+				$eventos[$i]->setPagamento($registro["pagamento"]);
+				$i++;
+			}
+		}
+
+		return $eventos;
 	}
 	
-	public function  selecionarListaAtividadePorCodigoEvento($cod_evento){
-		$sql = 'Select a.nome, b.cod_atividade_agenda, b.horario_inicio, b.data from Atividade a,
+	public function  selecionarAtividadesPorCodigoEvento($cod_evento){
+		$atividades = NULL;	
+		$sql = 'Select b.cod_atividade_agenda, a.nome, b.horario_inicio, b.data from Atividade a,
 				Atividade_Agenda b where b.cod_atividade = a.cod_atividade AND
 				a.cod_evento = '.$cod_evento;
-		
-		return FachadaConectorBD::getInstancia()->consultar($sql);
+		$registros = FachadaConectorBD::getInstancia()->consultar($sql);
+		$i = 0;
+		if (!is_null($registros)){
+			foreach ($registros as $registro){
+				$atividades[$i] = new AtividadeAgenda();
+				$atividades[$i]->setCodAtividadeAgenda($registro["cod_atividade_agenda"]);
+				$atividades[$i]->setNome($registro["horario_inicio"]);
+				$atividades[$i]->setNome($registro["horario_fim"]);
+				$atividades[$i]->setNome($registro["data"]);
+				$i++;
+			}
+		}
+		return $atividades;
 	}
 	
     public function selecionarVagasDisponiveisPorAtividade($cod_atividade){
@@ -23,13 +50,12 @@ class PersistenciaEvento extends InstanciaUnica{
                     aa.cod_atividade_agenda = '"  . $cod_atividade . "' AND i.status in ('Andamento', 'Confirmada')
                     AND h.cod_inscricao = i.cod_inscricao AND aa.cod_atividade_agenda = h.cod_atividade_agenda AND
                     a.cod_atividade = aa.cod_atividade GROUP BY a.vagas";
-            
-            $registros = FachadaConectorBD::getInstancia()->consultar($sql);
-            
-            return $registros;
+            $registro = FachadaConectorBD::getInstancia()->consultar($sql);
+
+            return $registro;
     }
     
-	public function selecionarEventoPorCodigo($cod_evento){
+	public function selecionarEventosPorCodigo($cod_evento){
 		$eventos = NULL;
 		$sql = 'Select * from Evento where cod_evento = '.$cod_evento;
 		$registros = FachadaConectorBD::getInstancia()->consultar($sql);
@@ -53,14 +79,17 @@ class PersistenciaEvento extends InstanciaUnica{
 		return $eventos;
 	}
     
-    public function selecionarAtividadePorCodigo($cod_atividade_agenda){
+    public function selecionarAtividadesPorCodigo($cod_atividade_agenda){
 		$atividades = NULL;
-		$sql = "Select nome from atividade a, atividade_agenda ag where a.cod_atividade = ag.cod_atividade AND ag.cod_atividade_agenda = ".$cod_atividade_agenda;
+		$sql = "Select a.cod_atividade, a.nome from atividade a, atividade_agenda ag where a.cod_atividade = ag.cod_atividade AND ag.cod_atividade_agenda = ".$cod_atividade_agenda;
 		$registros = FachadaConectorBD::getInstancia()->consultar($sql);
 		$i = 0;
 		if (!is_null($registros)){
-			foreach ($registros as $r){
-				$atividades = $r;
+			foreach ($registros as $registro){
+				$atividades[i] = new Atividade();
+				$atividades[i]->setCodAtividade($registro["cod_atividade"]);
+				$atividades[i]->setNome($registro["nome"]);
+				$i++;
 			}
 		}
 		return $atividades;
