6a2d250d375d28f3e4cff35f9b8320e187b03230
luispscarvalho@gmail.com <luispscarvalho@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-05 19:06:37 +0000

    + classes para listagem de atividades e agendamento de atividades

---
+++ trunk/aplicativo/classes/AtividadeAgenda.php
@@ -0,0 +1,53 @@
+<?php
+
+class Atividade {
+	private $cod_atividade_agenda;
+	private $nome;
+	private $data;
+	private $horario_inicio;
+	private $horario_fim;
+	
+	public function getCodAtividadeAgenda(){
+		return $this->$cod_atividade_agenda;
+	}
+	
+	public function getNome(){
+		return $this->nome;
+	}
+	
+	public function getData(){
+		return $this->data;
+	}
+	
+	public function getHorarioInicio(){
+		return $this->horario_inicio;
+	}
+	
+	public function getHorarioFim(){
+		return $this->horario_fim;
+	}
+
+	public function setCodAtividadeAgenda($cod_atividade_agenda){
+		$this->resumo = $cod_atividade_agenda;
+	}
+
+	
+	public function setNome($nome){
+		$this->nome = $nome;
+	}
+
+	public function setData($data){
+		$this->data = $data;
+	}
+
+	public function setHorarioInicio($horario_inicio){
+		$this->horario_inicio = $horario_inicio;
+	}
+	
+	public function setHorarioFim($horario_fim){
+		$this->horario_fim = $horario_fim;
+	}
+	
+}
+
+?>
