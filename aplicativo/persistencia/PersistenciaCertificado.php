1866d4c6fdff4bd502dceef5979f7269203189ef
luispscarvalho@gmail.com <luispscarvalho@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-05 19:11:48 +0000

    Fechamento da classe

--- aplicativo/persistencia/PersistenciaCertificado.php
+++ aplicativo/persistencia/PersistenciaCertificado.php
@@ -2,11 +2,10 @@
 
 class PersistenciaCertificado extends InstanciaUnica{
 	public function getBuscaInscricao($codUsuario, $codEvento){
-		//recupera codigo de inscrição
-		$sql= 'Select * from Inscricao where cod_USUARIO = '.$codUsuario,
-	
-	
-	//pegar nome do certificado
-	//pegar nome das atividades
-	//gravar certificado
+		//recupera codigo de inscricao
+		$sql= 'Select * from Inscricao where cod_USUARIO = '.$codUsuario;
+		//pegar nome do certificado
+		//pegar nome das atividades
+		//gravar certificado
+	}
 }
