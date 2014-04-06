dda4cb5bee66bb0a83253c2cbefed3679c76a0b8
cremildolima@gmail.com <cremildolima@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-06 17:44:04 +0000

    Organização dos arquivos para listagem de eventos e gerencia de atividade.

--- trunk/aplicativo/apresentacao/Evento/lista_eventos.php
+++ trunk/aplicativo/apresentacao/Evento/lista_eventos.php
@@ -1,17 +1,13 @@
-<?php
-require_once(dirname(__FILE__).'/../../config.php');
-require_once(FACHADAS.'FachadaUsuarioNivelAcesso.php');
-require_once(FACHADAS.'FachadaEvento.php');
-?>
-<form method="post" action="lista_atividade.php">
-	<?php 
-		$eventos = FachadaEvento::getInstancia()->listarEventos();
-		foreach ($eventos as $evento){
-			echo '<input type="radio" name="cod_evento" value="'.$evento->getCodEvento().'">'.$evento->getSigla().'-'.$evento->getNome().'<br/>';
-		}
-	?>
-	<input type="submit" value="Inscrever">
-</form>
-<?php 
+<?
+require_once (dirname(__FILE__) . '/../../config.php');
+require_once (APRESENTACAO . 'cabecalho.php');
+require_once (FACHADAS . 'FachadaUsuarioNivelAcesso.php');
+require_once (FACHADAS . 'FachadaEvento.php');
 
+$eventos = FachadaEvento::getInstancia() -> listarEventos();
+echo "<ul>";
+foreach ($eventos as $evento) {
+	 echo "<li><a href=\"" . URL . "apresentacao/Evento/evento.php?cod_evento=" . $evento->getCodEvento() . "\">".$evento->getNome() . $evento -> getStatus() . "</a></li>";
+}
+echo "</ul>";
 ?>
