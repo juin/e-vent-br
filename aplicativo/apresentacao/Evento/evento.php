3cb6f8656706f4fe8ebd65b86a84b3c5665ddb40
luispscarvalho@gmail.com <luispscarvalho@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-06 02:42:29 +0000

    Troca de POST por GET para acesso ao parametro de codigo do evento na URL

--- aplicativo/apresentacao/Evento/evento.php
+++ aplicativo/apresentacao/Evento/evento.php
@@ -3,20 +3,19 @@ require_once(dirname(__FILE__).'/../../config.php');
 require_once(APRESENTACAO.'cabecalho.php');
 require_once(FACHADAS.'FachadaEvento.php');
 
-$cod_evento = $_POST['cod_evento'];
+$cod_evento = $_GET['cod_evento'];
 echo "Informações do Evento: <br>";
-if($cod_evento!=NULL){
+if($cod_evento != NULL){
     $evento = FachadaEvento::getInstancia()->listarEventoPorCodigo($cod_evento);
     echo "Código do evento: " . $evento->getCodevento() . "<br>";
     echo $evento->getNome() . "<br>";
     echo $evento->getSigla() . "<br>";
-    echo "Data de Inicio: " . $evento->getDatainicio() ."<br>";
-    echo "Data de Fim: " . $evento->getDataFim() ."<br>";
+    echo "Data de Inicio: " . $evento->getDatainicio() . "<br>";
+    echo "Data de Fim: " . $evento->getDataFim() . "<br>";
     echo "<a href=\"".URL."apresentacao/Evento/lista_atividade.php?cod_evento=".$cod_evento."\">INSCREVER</a>";
-     echo "<br><br><a href='javascript:history.back(2)'>Clique aqui para voltar</a>";
-}else{
+    echo "<br><br><a href='javascript:history.back(2)'>Clique aqui para voltar</a>";
+} else {
     echo "Evento não encontrado.";
-    
 }
 
 echo "*************<br>";
