0e28d44ba851edc1e9816fe8db7b6c8f4e9f5aa7
luispscarvalho@gmail.com <luispscarvalho@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-06 00:21:03 +0000

    utilizacao do arquivo de configuracao para compor a url de navegacao para a tela de login

--- trunk/aplicativo/index.php
+++ trunk/aplicativo/index.php
@@ -1,7 +1,9 @@
 <?
+require_once ('config.php');
+
 session_start();
 if($_SESSION['UsuarioSessao'] == NULL){
-    header('location: apresentacao/Usuario/login.php');
+    header('location: '.URL.'apresentacao/Usuario/login.php');
 } else{
     echo "Seja Bem Vindo!";
 }
