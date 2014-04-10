69ea2c183dd5d8bc02c099ce1aae0658a96c0805
luispscarvalho@gmail.com <luispscarvalho@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-10 12:46:46 +0000

    Renomeação para o padrão >> lista_atividade -> lista_atividades Modificado "gerencia_evento": _POST -> _GET

--- trunk/aplicativo/apresentacao/Evento/lista_atividades.php
+++ trunk/aplicativo/apresentacao/Evento/lista_atividades.php
@@ -3,14 +3,14 @@ require_once(dirname(__FILE__).'/../../config.php');
 require_once(FACHADAS.'FachadaUsuarioNivelAcesso.php');
 require_once(FACHADAS.'FachadaEvento.php');
 ?>
-<h1>Atividades de <?php echo FachadaEvento::getInstancia()->listarEventoPorCodigo($_POST['cod_evento'])->getNome();?></h1>
+<h1>Atividades de <?php echo FachadaEvento::getInstancia()->listarEventoPorCodigo($_GET['cod_evento'])->getNome();?></h1>
 <form method="post" action="confirma_evento.php">
 	<?php 
-		$atividades = FachadaEvento::getInstancia()->listarAtividadesPorCodigoEvento($_POST['cod_evento']);
+		$atividades = FachadaEvento::getInstancia()->listarAtividadesPorCodigoEvento($_GET['cod_evento']);
 		foreach ($atividades as $atividade){
 			echo '<input type="checkbox" name="atv[]" value="'.$atividade->getCodAtividade().'">'.$atividade->getNome()." | ".$atividade->getVagas().'<br/>';
 		}
 	?>
-	<input type="hidden" name="cod_evento" value="<?php echo $_POST['cod_evento'];?>">
+	<input type="hidden" name="cod_evento" value="<?php echo $_GET['cod_evento'];?>">
 	<input type="submit" value="Inscrever">
 </form>
