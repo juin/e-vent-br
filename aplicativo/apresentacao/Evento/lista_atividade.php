77fa125a465787d1b0f3e3c4a34196a790dae641
lucasamparo.ti@gmail.com <lucasamparo.ti@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-10 15:46:18 +0000

    Processo Inscricao

---
+++ aplicativo/apresentacao/Evento/lista_atividade.php
@@ -0,0 +1,18 @@
+<?php
+require_once(dirname(__FILE__).'/../../config.php');
+require_once(FACHADAS.'FachadaUsuarioNivelAcesso.php');
+require_once(FACHADAS.'FachadaEvento.php');
+?>
+<h1>Atividades de <?php echo FachadaEvento::getInstancia()->listarEventoPorCodigo($_GET['cod_evento'])->getNome();?></h1>
+<form method="post" action="confirma_evento.php">
+	<?php 
+		$atividades = FachadaEvento::getInstancia()->listarAtividadeAgendaPorEvento($_GET['cod_evento']);
+		foreach ($atividades as $atividade){
+			echo '<input type="checkbox" name="atv[]" value="'.$atividade->getCodAtividadeAgenda().'">'.$atividade->getNome()." | 
+				  Data: ".$atividade->getData()." | Horario Início: ".$atividade->getHorarioInicio()."| Vagas: ".
+				  FachadaEvento::getInstancia()->listarAtividadePorCodigo($atividade->getCodAtividade())->getVagas().'<br/>';
+		}
+	?>
+	<input type="hidden" name="cod_evento" value="<?php echo $_POST['cod_evento'];?>">
+	<input type="submit" value="Inscrever">
+</form>
