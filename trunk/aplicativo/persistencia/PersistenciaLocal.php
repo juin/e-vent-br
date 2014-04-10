525ffc69d537eb0eb4ae189c612b4aa8cc9534d1
luispscarvalho@gmail.com <luispscarvalho@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-10 13:23:42 +0000

    Resolvido problema de require_once -> faltava referencia a instancia unica

--- trunk/aplicativo/persistencia/PersistenciaLocal.php
+++ trunk/aplicativo/persistencia/PersistenciaLocal.php
@@ -1,13 +1,14 @@
 <?php
-require_once (FACHADAS . 'FachadaConectorBD.php');
-require_once (CLASSES . 'Local.php');
+require_once(FACHADAS.'FachadaConectorBD.php');
+require_once(CLASSES.'InstanciaUnica.php');
+require_once(CLASSES.'Local.php');
 
 class PersistenciaLocal extends InstanciaUnica {
 
 	public function selecionarPorCodigo($cod_local) {
 		$locais = NULL;
 		$sql = "SELECT cod_local, nome, sigla, bloco FROM Local WHERE cod_local=" . $cod_local;
-		$registros = FachadaConectorBD::getInstancia() -> consultar($sql);
+		$registros = FachadaConectorBD::getInstancia()->consultar($sql);
 		$i = 0;
 		if (!is_null($registros)) {
 			foreach ($registros as $registro) {
