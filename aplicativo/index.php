1c150e8a173b6753232f62da6e5110b4ab925192
luispscarvalho@gmail.com <luispscarvalho@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-10 13:21:39 +0000

    Teste de nulidade refatorado: == NULL -> !isset($_SESSION

--- aplicativo/index.php
+++ aplicativo/index.php
@@ -2,7 +2,7 @@
 require_once ('config.php');
 
 session_start();
-if($_SESSION['UsuarioSessao'] == NULL){
+if(!isset($_SESSION['UsuarioSessao'])) {
     header('location: '.URL.'apresentacao/Usuario/login.php');
 } else{
     echo "Seja Bem Vindo!";
