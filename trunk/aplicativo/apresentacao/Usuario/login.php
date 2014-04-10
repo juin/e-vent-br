f7cc8251169e3fc380689a613a66f68a9db2bbdf
luispscarvalho@gmail.com <luispscarvalho@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-04-10 15:54:40 +0000

    + testes de utilizacao de javascript

--- trunk/aplicativo/apresentacao/Usuario/login.php
+++ trunk/aplicativo/apresentacao/Usuario/login.php
@@ -1,33 +1,38 @@
 <?
 require_once(dirname(__FILE__).'/../../config.php');
 require_once(FACHADAS.'FachadaUsuario.php');
+require_once(UTILIDADES);
 
-if(isset($_POST['login']) && ($_POST['senha'])){
-    $login=$_POST['login'];
-    $senha=$_POST['senha'];
-
-    if(($login) AND ($senha)) { //Ele entra nessa condição se as duas variáveis não estiverem vazia
-    
-        $usuario = FachadaUsuario::getInstancia()->validarAcesso($login,$senha);
-        
-        if ($usuario != NULL) {
-            session_start(); 
-            $_SESSION['tempo'] = time();
-            $_SESSION['usuario'] = $usuario;
-            header('location: '.URL.'apresentacao/index.php');
-        } else {
-            echo "Login ou Senha inválido.";
-        }
-    }
+$informarInvalido = false;
+$informarCamposNaoPreenchidos = false;
+if (isPostBack()) {
+	if(isset($_POST['login']) && ($_POST['senha'])){
+	    $login=$_POST['login'];
+	    $senha=$_POST['senha'];
+	
+	    if(($login) && ($senha)) { //Ele entra nessa condição se as duas variáveis não estiverem vazia
+	        $usuario = FachadaUsuario::getInstancia()->validarAcesso($login, $senha);
+	        
+	        if ($usuario != NULL) {
+	            session_start(); 
+	            $_SESSION['tempo'] = time();
+	            $_SESSION['usuario'] = $usuario;
+	            header('location: '.URL.'apresentacao/index.php');
+	        } 
+	    }
+	    $informarInvalido = true;
+	} else {
+		$informarCamposNaoPreenchidos = true;
+	}
 }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
+<script src="<?echo SCRIPTS.'Usuario/login.js'?>"></script>
 <title>Login | e-Vent-br</title>
 </head>
-
 <body>
 <form method="post" action="login.php">
     <label>Login:</label>
@@ -39,3 +44,11 @@ if(isset($_POST['login']) && ($_POST['senha'])){
 </form>
 </body>
 </html>
+<?
+if($informarInvalido) {
+	echo "<script>informarLoginInvalido();</script>";
+}
+if($informarCamposNaoPreenchidos){
+	echo "<script>informarCamposNaoPreenchidos();</script>";
+}
+?>
