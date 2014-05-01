ca2dabf1c97192c3d0ad2b04a61df2d188d6c9e2
cremildolima@gmail.com <cremildolima@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-05-01 22:48:57 +0000

    Adicionado a classe AtividadeValor que une os dados das tabelas Atividade_Valor e Atividade_Tipo. Criada também a FachadaAtividade e PersistenciaAtividade. Em breve, vamos tirar todos os metódos referentes as atividades para a fachada e persistencia correspondente. No arquivo lista_atividades.php foi adicionado a exibição dos valores das atividades para o evento selecionado.

--- aplicativo/apresentacao/Evento/lista_atividades.php
+++ aplicativo/apresentacao/Evento/lista_atividades.php
@@ -2,29 +2,21 @@
 require_once(dirname(__FILE__).'/../../config.php');
 require_once(dirname(__FILE__).'/../../utilidades.php');
 require_once(FACHADAS.'FachadaEvento.php');
+require_once(FACHADAS.'FachadaAtividade.php');
 ?>
 <h1>Atividades de <?php echo FachadaEvento::getInstancia()->listarEventoPorCodigo($_GET['cod_evento'])->getNome();?></h1>
 <form method="post" action="confirmacao_evento.php">
 	<?php 
+		$atividades_tipo = FachadaAtividade::getInstancia()->listarTiposAtividadePorEvento($_GET['cod_evento']);
+		
+		foreach ($atividades_tipo as $atividade_tipo) {
+			echo $atividade_tipo->getNome() . $atividade_tipo->getValorEstudante() ."<br>";
+		}
 		$atividades = FachadaEvento::getInstancia()->listarAtividadeAgendaPorEvento($_GET['cod_evento']);
 		foreach ($atividades as $atividade){
-			$disponiveis =  FachadaEvento::getInstancia()->listarVagasDisponiveisPorAtividade($atividade->getCodAtividadeAgenda());
-			if ($disponiveis > 0) 
-			{ 
-				echo '<input type="checkbox" name="atv[]" value="'.$atividade->getCodAtividadeAgenda().'">'.$atividade->getNome()." | 
-					  Data: ".arrumaData($atividade->getData())." | Horario Inicio: ".$atividade->getHorarioInicio()."| Total de vagas: ".
-					  FachadaEvento::getInstancia()->listarAtividadePorCodigo($atividade->getCodAtividade())->getVagas().
-				      "| Vagas disponiveis: ".$disponiveis.
-				     "<br/>";
-			}
-			else 
-			{
-				echo $atividade->getNome()." | 
-					 Data: ".arrumaData($atividade->getData())." | Horario Inicio: ".$atividade->getHorarioInicio()."| Total de vagas: ".
-					 FachadaEvento::getInstancia()->listarAtividadePorCodigo($atividade->getCodAtividade())->getVagas().
-				     "| ESGOTADO! ".
-				     "<br/>";
-			}
+			echo '<input type="checkbox" name="atv[]" value="'.$atividade->getCodAtividadeAgenda().'">'.$atividade->getNome()." | 
+				  Data: ".arrumaData($atividade->getData())." | Horario Inicio: ".$atividade->getHorarioInicio()."| Vagas: ".
+				  FachadaEvento::getInstancia()->listarAtividadePorCodigo($atividade->getCodAtividade())->getVagas().'<br/>';
 		}
 	?>
 	<input type="hidden" name="cod_evento" value="<?php echo $_GET['cod_evento'];?>">
