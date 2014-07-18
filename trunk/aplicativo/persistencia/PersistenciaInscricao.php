4e083ba05d59aeb44944eb414bbd1218460f322c
luispscarvalho <luispscarvalho@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-07-18 02:47:53 +0000

    removido metodo antigo de contagem de vagas a partir da agenda de atividades (deve considerar atividades)

--- trunk/aplicativo/persistencia/PersistenciaInscricao.php
+++ trunk/aplicativo/persistencia/PersistenciaInscricao.php
@@ -8,9 +8,9 @@ class PersistenciaInscricao extends InstanciaUnica {
     
     //Seleciona todas as inscrições do usuário
     public function selecionarInscricoesPorUsuario($cod_usuario){
-        $sql = "Select i.cod_inscricao, e.cod_evento, e.nome, i.data_hora_inscricao, i.status " .
-        		"From Inscricao i, Evento e Where i.cod_usuario='" . $cod_usuario . "' " .
-        				"and e.cod_evento = i.cod_evento Order by i.cod_inscricao Desc";
+        $sql = "Select i.cod_inscricao, e.cod_evento, e.nome, i.data_hora_inscricao, i.forma_pagamento, i.status
+        	    From Inscricao i, Evento e Where i.cod_usuario='" . $cod_usuario . "'
+        		and e.cod_evento = i.cod_evento Order by i.cod_inscricao Desc";
         // seleciona os 5 ultimos registros de inscricao
         $registros = FachadaConectorBD::getInstancia()->consultarComLimite($sql,5);
         $inscricoes = null;
@@ -19,11 +19,11 @@ class PersistenciaInscricao extends InstanciaUnica {
 			foreach ($registros as $registro){
 				$inscricoes[$i] = new Inscricao();
 				$inscricoes[$i]->setCodInscricao($registro["cod_inscricao"]);
-				$inscricoes[$i]->setDataHora($registro["data_hora"]);
-				$inscricoes[$i]->setFormaPagamento($registro["forma_pagamento"]);
-				$inscricoes[$i]->setStatus($registro["status"]);
 				$inscricoes[$i]->setCodEvento($registro["cod_evento"]);
 				$inscricoes[$i]->setNomeEvento($registro["nome"]);
+				$inscricoes[$i]->setDataHora($registro["data_hora_inscricao"]);
+				$inscricoes[$i]->setFormaPagamento($registro["forma_pagamento"]);
+				$inscricoes[$i]->setStatus($registro["status"]);
 				$i++;
 			}
 		}
