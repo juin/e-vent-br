8a5181eddbc78c60c5a6a63600484706c4345568
cremildolima@gmail.com <cremildolima@gmail.com@1c4506dc-9845-082d-9e46-9dff84d7c2d8>
2014-09-19 04:46:39 +0000

    Implementando upload de imagens.

--- aplicativo/utilidades.php
+++ aplicativo/utilidades.php
@@ -1,5 +1,4 @@
 <?php
-
 // verifica se ocorreu um envio do formulario atraves do metodo POST
 function isPostBack() { 
     return ($_SERVER['REQUEST_METHOD'] == 'POST');
@@ -19,38 +18,36 @@ function arrumaHora($hora){
 	return $hora;
 }
 
-function uploadImagem($arguivo){
+function uploadImagem($arquivo){
 	
-$extensoesPermitidas = array("gif", "jpeg", "jpg", "png");
-$temp = explode(".", $_FILES["file"]["name"]);
-$extensao = end($temp);
-$retorno = "";
+	$extensoesPermitidas = array("gif", "jpeg", "jpg", "png");
+	$temp = explode(".", $arquivo["name"]);
+	$extensao = end($temp);
+	$retorno = null;
 
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/jpg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/x-png")
-|| ($_FILES["file"]["type"] == "image/png"))
-&& ($_FILES["file"]["size"] < 20000)
-&& in_array($extensao, $extensoesPermitidas)) {
-  if ($_FILES["file"]["error"] > 0) {
-    $retorno = "Código Retornado: " . $_FILES["file"]["error"] . "<br>";
-  } else {
-    $retorno = "Upload: " . $_FILES["file"]["name"] . "<br>";
-    $retorno .= "Tipo: " . $_FILES["file"]["type"] . "<br>";
-    $retorno .= "Tamanho: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    $retorno .= "Arquivo Temporário: " . $_FILES["file"]["tmp_name"] . "<br>";
-    if (file_exists("/apresentacao/Evento/upload/" . $_FILES["file"]["name"])) {
-      echo $_FILES["file"]["name"] . " já existe. ";
-    } else {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "/apresentacao/Evento/upload/" . $_FILES["file"]["name"]);
-      echo "Armazenado em: " . "/apresentacao/Evento/upload/" . $_FILES["file"]["name"];
-    }
-  }
-} else {
-  echo "Arquivo inválido";
-}
+	if ((($arquivo["type"] == "image/gif")
+	|| ($arquivo["type"] == "image/jpeg")
+	|| ($arquivo["type"] == "image/jpg")
+	|| ($arquivo["type"] == "image/pjpeg")
+	|| ($arquivo["type"] == "image/x-png")
+	|| ($arquivo["type"] == "image/png"))
+	&& ($arquivo["size"] < 90000000)
+	&& in_array($extensao, $extensoesPermitidas)) {
+		if ($arquivo["error"] > 0) {
+	    	$retorno["erro"] = $arquivo["error"];
+		} else {
+		    if (file_exists(APRESENTACAO."/Evento/upload/" . $arquivo["name"])) {
+		      $retorno["existe"] = true;
+		    } else {
+		      move_uploaded_file($arquivo["tmp_name"],
+		      APRESENTACAO."/Evento/upload/" . $arquivo["name"]);
+		      $retorno["url"] = URL."apresentacao/Evento/upload/" . $arquivo["name"];
+		    	}
+			}
+	} else {
+		$retorno["extensao"] = false;
+		}
+
+	return $retorno;
 }
 ?>
